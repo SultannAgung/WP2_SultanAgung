a11488c615def55d2042cbc6ec120acbb58df213
SultannAgung <sultanag2112@gmail.com>
2023-09-29 16:25:58 +0700

    Instalasi CI3 dan menambahkan Contoh1.php pada controller

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+ public function index()
+ {
+ echo "<h1>Perkenalkan</h1>";
+ echo"Nama saya Sultan Agung
+ Saya tinggal di Sindang barang, Bogor
+ olah raga yang saya sukai adalah
+ sepak bola.";
+ }
+}
---
+++ application/views/index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Personal Homepage</title>
+    <style>
+
+        body {
+            font-family: Arial, sans-serif;
+            background-color: #6495ED;
+        }
+
+        .container {
+            max-width: 800px;
+            margin: 0 auto;
+            padding: 20px;
+            background-color: #ffffff;
+            box-shadow: 0px 0px 10px 0px rgba(0, 0, 0, 0.1);
+            margin-top: 50px;
+            border-radius: 10px;
+        }
+    </style>
+</head>
+
+<body>
+    <div class="container">
+    <img src="https://i.pinimg.com/564x/cf/5e/4a/cf5e4aaf8dbbed0c485fa18eee5d7ad9.jpg" alt="Foto Profil" style="max-width: 150px; border-radius: 50%;">
+        <h1>Welcome to My Personal Homepage!</h1>
+        <p>Hallo saya Sultan Agung. Ini adalah website pribadi saya.</p>
+        <h2>About Me</h2>
+        <p>Hi, saya Sultan Agung Mahasiswa UBSI yang berdomisili di Bogor. Saat ini saya sedang belajar membuat Website.</p>
+        <p>Tujuan saya kuliah adalah untuk mencari ilmu yang bermanfaat untuk diri sendiri dan orang lain,tentu saja setelah lulus saya harap bisa bekerja di perusahaan berbasis IT.</p>
+        <h2>Contact</h2>
+        <p>Email: <a href="mailto:sultanag2112@gmail.com">sultanag2112@gmail.com</a><br>
+           Phone: <a href="tel:0823281723814">0823281723814</a></p>
+
+    </div>
+</body>
+
+</html>
