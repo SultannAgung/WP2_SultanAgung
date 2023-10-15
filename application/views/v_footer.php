191c21de0d49068e656f9f2dc353f541938888c9
SultannAgung <sultanag2112@gmail.com>
2023-10-15 18:58:38 +0700

    menambahkan materi latihan pertemuan 3

---
+++ application/views/v_footer.php
@@ -0,0 +1,7 @@
+<footer>
+ <a href=<?php echo 
+base_url().'index.php/web' ?>>RentalBuku</a>
+ </footer>
+ </div>
+</body>
+</html>
---
+++ application/views/v_news.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<body>
+    <section>
+        <div class="container">
+ <h1>Breaking News: Marc Marquez Resmi Gabung Gresini Ducati</h1>
+ <img src="https://img.okezone.com/content/2023/10/12/38/2899885/breaking-news-marc-marquez-resmi-gabung-gresini-ducati-MrSaUoJMHw.jpg" alt="breaking news">
+ <p><strong>MANDALIKA</strong>  Bintang MotoGP, Marc Marquez, akhirnya resmi bakal pindah tim tahun depan dari Repsol Honda. Dia sudah dipastikan akan bergabung dengan Gresini Ducati mulai MotoGP 2024 mendatang.</p>
+ <p>Hal tersebut diumumkan oleh Gresini lewat Twitter mereka, @GresiniRacing, pada Kamis (12/10/2023). Mereka mengatakan bahwa The Baby Alien -julukan Marc- sudah resmi hijrah ke tim mereka mulai musim depan.</p>
+ <img src="https://img.okezone.com/okz/500/library/images/2023/10/12/uwe6xwawsht2932aszaa_17856.jpg" alt="breaking news">
+<p>“Ini Resmi! @marcmarquez93 bergabung dengan Tim Gresini MotoGP #GresiniFamily,” cuit Gresini sembari menampilkan foto Marc tengah tersenyum lebar.</p>
+<p>Dengan begitu, pembalap asal Spanyol tersebut bakal berduet dengan adiknya, Alex Marquez, yang sudah lebih dulu bergabung dengan tim arahan Nadia Padovani itu. Dia akan menggantikan Fabio Di Giannantonio.</p>
+<p>Lebih lanjut lagi, Honda rupanya meminta Marquez untuk tetap bungkam mengenai masa depannya hingga akhir musim tahun ini. Sedangkan konferensi pers khusus tersebut jika tidak ada perubahan akan dihadiri oleh Alex Marquez, Augusto Fernandez , Enea Bastianini, dan pembalap Moto2 Pedro Acosta.</p>
+<p>Marquez pertama kali debut dengan tim asal Jepang tersebut pada 2013 silam usai naik kelas dari Moto2. Selama menunggangi Repsol Honda, Marc Marquez telah mencatatkan rekor fantastis, yakni enam kali juara dunia MotoGP pada 2013, 2014, 2016,2017, 2018, dan 2019.</p>
+    </div>
+</body>
+</section>
